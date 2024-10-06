0b76dbfece49a7187f0c06a41bd52590645f09ce
Ecstassys <ecstasys.exe@gmail.com>
2024-10-06 16:31:41 +0530

    Updated server.js and other project configuration files

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-const express = require('express')
+const express = require('express');
+const { connectToDatabase } = require('./config/database'); 
+
 const app = express();
+const PORT = 8000;
+
 
 app.use(express.json());
 
+async function startServer() {
+  try {
+    
+    await connectToDatabase();
+    
+   
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    
+    console.error("Error connecting to MongoDB. Server not started.", error.message);
+    process.exit(1); 
+  }
+}
 
-app.get('/', (req, res) => {
-    res.send("This is PodAcademy Backend")
 
-});
-
-
-const PORT = 8000;
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`);
-})
-
+startServer();
